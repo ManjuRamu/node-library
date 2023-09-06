0f7b834f373d0ead9b55d91671a893fc70f9741c
MANJU <MANJU@>
2023-09-07 00:16:36 +0530

    #end streams and close files

--- streams/writeableStreams/07-streams-writer-with-drain-event.js
+++ streams/writeableStreams/07-streams-writer-with-drain-event.js
@@ -6,12 +6,20 @@
   let i = 0;
   function writeStream() {
     while (i< 100000000) {
+      // Pause the stream write if inter-buffer is full i.e writableLength >=  writableHighWaterMark
       if (!streams.write(Buffer.from(` ${i} `)))
        break;
-      i++;
+       i++;
+       //end the stream and close the fileHandler 
+      if (99999999) {
+        streams.end()
+        fileHandler.close()
+      }
     }
   }
   writeStream()
+  
+   // after free up the internal buffer resume stream write
   streams.on('drain', ()=>{
     writeStream()
   })
--- streams/writeableStreams/08-all-the-above.js
+++ streams/writeableStreams/08-all-the-above.js
@@ -108,20 +108,28 @@
 // })()
 
 // streams write pretty enough creates 943 Mb file without memory leaks
-// (async function () {
-//   const fs = require("fs/promises")
-//   const fileHandler  = await fs.open("writeManyStreamsDrain.txt",'w')
-//   const streams = fileHandler.createWriteStream()
-//   let i = 0;
-//   function writeStream() {
-//     while (i< 100000000) {
-//       if (!streams.write(Buffer.from(` ${i} `)))
-//        break;
-//       i++;
-//     }
-//   }
-//   writeStream()
-//   streams.on('drain', ()=>{
-//     writeStream()
-//   })
-// })()
+(async function () {
+  const fs = require("fs/promises")
+  const fileHandler  = await fs.open("writeManyStreamsDrain.txt",'w')
+  const streams = fileHandler.createWriteStream()
+  let i = 0;
+  function writeStream() {
+    while (i< 100000000) {
+      // Pause the stream write if inter-buffer is full i.e writableLength >=  writableHighWaterMark
+      if (!streams.write(Buffer.from(` ${i} `)))
+       break;
+       i++;
+       //end the stream and close the fileHandler 
+      if (99999999) {
+        streams.end()
+        fileHandler.close()
+      }
+    }
+  }
+  writeStream()
+  
+   // after free up the internal buffer resume stream write
+  streams.on('drain', ()=>{
+    writeStream()
+  })
+})()
