abdee070190a9c0722c1ab98e528736313e2f7cf
MANJU <MANJU@>
2023-11-17 00:36:02 +0530

    #swagger implementation

--- swagger/server.js
+++ swagger/server.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const cors = require('cors')
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const { router } = require('./routes/users');
 const swagger  = require('./swagger.json')
 const YAML = require('yaml')
 const fs = require("fs")
+
+
 const file  = fs.readFileSync('./swagger.yml', 'utf8')
 const swaggerDocument = YAML.parse(file)
 const app = express();
+app.use(cors({
+  origin:"*"
+}))
 app.use(express.json())
 app.use('/swagger-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/users', router)
-app.listen(3000, ()=>{
-console.log("server listen on : ", 3000)
+const server = app.listen(3000, ()=>{
+console.log( console.log(`http://localhost:${3000}`, server.address()))
 })
